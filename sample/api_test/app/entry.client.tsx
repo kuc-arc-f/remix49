584d4c38fa3380591a4d439838d1dd2c72ca1f86
中島浩二 <knaka0209@gmail.com>
2024-06-08 18:15:34 +0900

    up, 0608a4

--- sample/api_test/app/entry.client.tsx
+++ sample/api_test/app/entry.client.tsx
@@ -8,6 +8,8 @@ import { RemixBrowser } from "@remix-run/react";
 import { startTransition, StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
 
+//import './index.css';
+//
 startTransition(() => {
   hydrateRoot(
     document,
--- sample/api_test/app/root.tsx
+++ sample/api_test/app/root.tsx
@@ -14,6 +14,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link href="/index.css" rel="stylesheet" />
+        <script src="https://cdn.tailwindcss.com"></script>
         <Meta />
         <Links />
       </head>
